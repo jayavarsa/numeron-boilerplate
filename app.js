c6bc28b56e17a8b9f1cd7f5c481f56777f9ec41b
jayavarsan <jayavarsan.r@kalvium.community>
2024-01-13 21:12:30 +0530

    jschnage

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // Iteration 1: Make the Play button functional.
+
+var play_button = document.getElementById("play-button")
 // Description: When the Play button is clicked the game page should be displayed.
+function locator(){
+    window.location = './game.html'
+}
+play_button.addEventListener('click' ,locator)
--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+var total_score = localStorage.getItem("score")
+
+var score = document.getElementById("score-board")
+var play_button = document.getElementById("play-again-button")
+
+score.innerHTML = total_score;
+
+play_button.addEventListener('click',function(){
+    window.location.href="./game.html";
+})
